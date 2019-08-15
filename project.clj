c1515f02c6f8b944890a450a31071f7d52c0f45c
Vasiliy Avdiushkin <dixelt@gmail.com>
2019-08-15 17:15:25 +0200

    publish websocket version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kafkus "0.1.0-alpha"
+(defproject kafkus "websocket"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.async "0.4.490"]
